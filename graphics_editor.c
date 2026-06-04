0c4341d1be2c729130f6a41bd94dc076d79050b5
S.Deekshitha <sdeekshi9595@gmail.com>
2026-06-04 18:52:47 +0530

    Day 2 - Rectangle drawing

--- graphics_editor.c
+++ graphics_editor.c
@@ -7,26 +7,43 @@ char canvas[ROWS][COLS];
 
 void initCanvas()
 {
-    int i, j;
+    int i,j;
 
-    for(i = 0; i < ROWS; i++)
+    for(i=0;i<ROWS;i++)
     {
-        for(j = 0; j < COLS; j++)
+        for(j=0;j<COLS;j++)
         {
-            canvas[i][j] = '_';
+            canvas[i][j]='_';
         }
+    }
+}
+
+void drawRectangle(int row,int col,int width,int height)
+{
+    int i;
+
+    for(i=col;i<col+width;i++)
+    {
+        canvas[row][i]='*';
+        canvas[row+height-1][i]='*';
+    }
+
+    for(i=row;i<row+height;i++)
+    {
+        canvas[i][col]='*';
+        canvas[i][col+width-1]='*';
     }
 }
 
 void displayCanvas()
 {
-    int i, j;
+    int i,j;
 
-    for(i = 0; i < ROWS; i++)
+    for(i=0;i<ROWS;i++)
     {
-        for(j = 0; j < COLS; j++)
+        for(j=0;j<COLS;j++)
         {
-            printf("%c", canvas[i][j]);
+            printf("%c",canvas[i][j]);
         }
         printf("\n");
     }
@@ -35,6 +52,9 @@ void displayCanvas()
 int main()
 {
     initCanvas();
+
+    drawRectangle(2,5,10,5);
+
     displayCanvas();
 
     return 0;
